0cb7d4f35f236389b65ce9d483a0e5d3ea45cc35
PiotrSiryk <54096666+PiotrSiryk@users.noreply.github.com>
2021-05-24 21:43:31 +0200

    Create Test.vue

---
+++ Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+    <UniversalDialog v-model="test"> </UniversalDialog>
+    <v-btn @click="test = true">Open Dialog</v-btn>
+  </div>
+</template>
+
+<script lang='ts'>
+import UniversalDialog from '@/components/UniversalDialog.vue';
+
+export default {
+  data() {
+    return { test: false };
+  },
+
+  components: { UniversalDialog }
+};
+</script>
